c70efc21b9c97c441d917005691d7dc4bc237419
Noah Rothgaber <noahrothgaber@gmail.com>
2023-06-14 14:05:53 -0400

    Added weather api call to manager class, updated json file with proper syntax, and templated the api weather call for my area.

--- Scripts/weather_caller.py
+++ Scripts/weather_caller.py
@@ -24,6 +24,11 @@ ontario_in_canada = reg.ids_for('Ontario', country='CA', matching='exact')  # on
 # Ontario in the state of New York
 ontario_in_ny = reg.ids_for('Ontario', country='US', state='NY', matching='exact')  # one result: [(5129887, 'Ontario', 'US', 'NY', 43.220901, -77.283043)]
 '''
+from subprocess import run
+
+
+def print_with_lolcat(print_val):
+    run(["lolcat"], input=print_val, text=True)
 
 
 class WeatherCaller:
@@ -31,12 +36,47 @@ class WeatherCaller:
     owm = OWM(open_weather_key)
     config_dict = get_config_from("./config/owm_config.json")
     reg = owm.city_id_registry()
+    weather_manager_obj = owm.weather_manager()
+    country = ''
+    city = ''
+    state = ''
+    zip = ''
+    lat = None
+    lon = None
+    weather_read = None
+    list_of_locations = None
+    location_coord = None
 
     def __init__(self):
         self.load_key()
         self.owm = OWM(self.open_weather_key, self.config_dict)
+        self.parse_user_info()
+        self.reg = self.owm.city_id_registry()
+        self.list_of_locations = self.reg.locations_for \
+                (
+                city_name=self.city,
+                state=self.state, country=self.country,
+                matching='exact'
+            )
+        self.location_coord = self.list_of_locations[0]
+        self.lat = self.location_coord.lat
+        self.lon = self.location_coord.lon
+        self.weather_read = self.weather_manager_obj.one_call \
+            (self.lat, self.lon)
+        print_with_lolcat(self.weather_read)
 
     def load_key(self):
         with open("./config/dont_commit", 'r') as file:
             for line in file:
                 self.open_weather_key = line
+
+    def parse_user_info(self):
+        with open('./config/info.txt', 'r') as file:
+            for line in file:
+                if line[: 6] == "Zip = ":
+                    self.zip = line[6:].strip()
+                elif line[: 7] == "City = ":
+                    self.city = line[7:].strip()
+                elif line[: 10] == "Country = ":
+                    self.country = line[10:].strip()
+                    print(self.country)
--- welcome_manager.py
+++ welcome_manager.py
@@ -1,9 +1,9 @@
-from Scripts import dad_joke, setup_script
-
+from Scripts import dad_joke, setup_script, weather_caller
 
 class WelcomeManager():
     def __init__(self):
         setup = setup_script.SetupHost()
         dad = dad_joke.DadJokeHandler()
+        weather = weather_caller.WeatherCaller()
 
 welcome_manager = WelcomeManager()
